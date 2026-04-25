6982af709a25b7c927016410f98e170ec910a294
Jessica Santos <jessiicansantos@gmail.com>
2026-04-25 18:07:58 -0300

    Fix: erro ao startar o projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
+import animate from "tailwindcss-animate"
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: ["class"],
+  content: [
+    './pages**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        // ... outras cores (secondary, destructive, etc)
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      }
+    },
   },
-  plugins: [],
+  plugins: [animate],
 }
-
